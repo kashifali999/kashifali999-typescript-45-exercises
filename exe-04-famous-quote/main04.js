01bc88c1f786fe30ee80dd74d1c4f24f7da3e059
Kashif Ali <kashif_ali999@yahoo.com>
2024-04-20 00:17:23 +0500

    initial commit

---
+++ exe-04-famous-quote/main04.js
@@ -0,0 +1,3 @@
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks: Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+//If we are suppose to add double qoutation marks within our string than we may use single qoutation marks around whole string to display or vice versa
+console.log("Albert Einstein once said, \u201CA person who never made a mistake never tried anything new.\u201D");
