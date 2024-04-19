de270cbf7f1fa3453cf523a0bee20f62d3ad006c
Kashif Ali <kashif_ali999@yahoo.com>
2024-04-20 00:57:37 +0500

    initial commit

--- exe-06-stripping-names/main06.js
+++ exe-06-stripping-names/main06.js
@@ -1,9 +1,14 @@
 // Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
 //============================================================================================================================================
 // "\t" for new tab and "\n" for new line
-var whiteSpacedName = "\t \n Kashif Ali \n \t";
-var strippedName = "Rashid Ali";
+var firstName = "\t Kashif Ali \t";
+var secondName = "\n Rashid Ali \n";
+var thirdName = "\n \t Shoaib Ali \t \n";
 console.log("Name with white spaces");
-console.log(whiteSpacedName);
+console.log(firstName);
+console.log(secondName);
+console.log(thirdName);
 console.log("Name after stripping white spaces");
-console.log(strippedName);
+console.log(firstName.trim());
+console.log(secondName.trim());
+console.log(thirdName.trim());
--- exe-06-stripping-names/main06.ts
+++ exe-06-stripping-names/main06.ts
@@ -1,11 +1,16 @@
 // Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
 //============================================================================================================================================
 // "\t" for new tab and "\n" for new line
-let whiteSpacedName: string = "\t \n Kashif Ali \n \t"
-let strippedName: string = "Rashid Ali"
+let firstName: string = "\t Kashif Ali \t"
+let secondName: string = "\n Rashid Ali \n"
+let thirdName: string = "\n \t Shoaib Ali \t \n"
 
 console.log("Name with white spaces")
-console.log(whiteSpacedName)
+console.log(firstName)
+console.log(secondName)
+console.log(thirdName)
 
 console.log("Name after stripping white spaces")
-console.log(strippedName)
+console.log(firstName.trim())
+console.log(secondName.trim())
+console.log(thirdName.trim())
