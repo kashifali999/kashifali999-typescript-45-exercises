dc30bbf8508081e7f4a3cf64c6d1e8d600c08aab
Kashif Ali <kashif_ali999@yahoo.com>
2024-04-20 01:06:04 +0500

    initial commit

---
+++ exe-07-number-eight/main07.js
@@ -0,0 +1,24 @@
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+//======================================================================================================================================
+//I am suppose to use four basic arithmetic operations in such a way that each operation produces figure eight "8" as answer.
+//I also added "\n" for clear output.
+//lets go for addition
+var num1 = 6;
+var num2 = 2;
+console.log("\nAddition");
+console.log(num1 + num2);
+//lets go for subtraction
+var num3 = 14;
+var num4 = 6;
+console.log("\nSubtraction");
+console.log(num3 - num4);
+//lets go for multiplication
+var num5 = 2;
+var num6 = 4;
+console.log("\nMultiplication");
+console.log(num5 * num6);
+//lets go for division
+var num7 = 40;
+var num8 = 5;
+console.log("\nDivision");
+console.log(num7 / num8);
---
+++ exe-07-number-eight/main07.ts
@@ -0,0 +1,27 @@
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+//======================================================================================================================================
+//I am suppose to use four basic arithmetic operations in such a way that each operation produces figure eight "8" as answer.
+//I also added "\n" for clear output.
+//lets go for addition
+let num1: number = 6
+let num2: number = 2
+console.log("\nAddition")
+console.log(num1 + num2)
+
+//lets go for subtraction
+let num3: number = 14
+let num4: number = 6
+console.log("\nSubtraction")
+console.log(num3 - num4)
+
+//lets go for multiplication
+let num5: number = 2
+let num6: number = 4
+console.log("\nMultiplication")
+console.log(num5 * num6)
+
+//lets go for division
+let num7: number = 40
+let num8: number = 5
+console.log("\nDivision")
+console.log(num7 / num8)
