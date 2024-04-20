5902f29247a665b7e5088dfdd76b1c64c01cf58a
Kashif Ali <kashif_ali999@yahoo.com>
2024-04-20 11:27:38 +0500

    initial commit

---
+++ exe-10-adding-comments/main10.js
@@ -0,0 +1,11 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//=================================================================================================
+/*  Auther   Kashif Ali
+    Date     20-04-2024
+*/
+//1st program
+var message1 = "Hello World";
+console.log("My first program was ".concat(message1));
+//2nd program
+var message2 = "My name is Kashif Ali";
+console.log("My second program was ".concat(message2));
---
+++ exe-10-adding-comments/main10.ts
@@ -0,0 +1,13 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//=================================================================================================
+
+/*  Auther   Kashif Ali
+    Date     20-04-2024
+*/
+
+//1st program
+let message1:string = "Hello World"
+console.log(`My first program was ${message1}`)
+//2nd program
+let message2:string = "My name is Kashif Ali"
+console.log(`My second program was ${message2}`)
