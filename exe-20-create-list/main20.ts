1f19bcce90f602dd7583342150d764db44cffbca
Kashif Ali <kashif_ali999@yahoo.com>
2024-04-21 04:46:52 +0500

    initial commit

---
+++ exe-20-create-list/main20.ts
@@ -0,0 +1,4 @@
+/*Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.*/
+//===================================================================================================================================
+let cities:string[] = ["Lahore", "Karachi", "Peshawer", "Quetta", "Islamabad"]
+cities.map((list)=>(console.log(list)))
