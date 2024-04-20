80cc0fe3d392152c5a1204976d873f3c9c720dab
Kashif Ali <kashif_ali999@yahoo.com>
2024-04-21 02:56:19 +0500

    initial commit

---
+++ exe-19-dinner-guests/main19.ts
@@ -0,0 +1,4 @@
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+//==================================================================================================================================
+let prevoiusGuests:string[] = ["Rashid", "Shoaib", "Ahmad", "Abdullah", "Kashif", "Umar"]
+console.log(prevoiusGuests.length + " guests are invited for dinner");
