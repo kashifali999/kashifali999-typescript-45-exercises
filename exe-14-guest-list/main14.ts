2ad2dcd7d5001dccf87fef7974d44901cf43c50e
Kashif Ali <kashif_ali999@yahoo.com>
2024-04-20 14:01:05 +0500

    initial commit

---
+++ exe-14-guest-list/main14.ts
@@ -0,0 +1,4 @@
+// Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+//=============================================================================================================
+let guestList:string[] = ["Rashid", "Shoaib", "Ahmad"]
+guestList.map((invitedGuests)=>(console.log(`Dear, ${invitedGuests} I would like to invite you for dinner tonight`)))
