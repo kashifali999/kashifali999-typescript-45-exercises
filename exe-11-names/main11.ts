88bc65282013033f48d920677274e7b76662d51e
Kashif Ali <kashif_ali999@yahoo.com>
2024-04-20 11:38:53 +0500

    initial commit

---
+++ exe-11-names/main11.ts
@@ -0,0 +1,7 @@
+// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+//=====================================================================================================
+let friendArray:string [] = ["Rashid", "Shoaib", "Ahmad", "Abdullah"]
+console.log(friendArray[0])
+console.log(friendArray[1])
+console.log(friendArray[2])
+console.log(friendArray[3])
