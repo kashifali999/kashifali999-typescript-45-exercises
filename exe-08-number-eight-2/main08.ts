149eb47937c989cc2451a4e867126e59344327a3
Kashif Ali <kashif_ali999@yahoo.com>
2024-04-20 11:13:01 +0500

    initial commit

---
+++ exe-08-number-eight-2/main08.ts
@@ -0,0 +1,9 @@
+/* You should create four lines that look like this:
+console.log(5 + 3)
+Your output should simply be four lines with the number 8 appearing once on each line.*/
+//=======================================================================================
+//I am suppose to write four lines code each displaying "8" as result.
+console.log(5+3)
+console.log(16-8)
+console.log(4*2)
+console.log(40/5)
