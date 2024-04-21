92e554d08f5b9d7dcd7b71393c3db7f109d5c66d
Kashif Ali <kashif_ali999@yahoo.com>
2024-04-21 17:29:48 +0500

    initial commit

---
+++ exe-25-alien-colors/main25.js
@@ -0,0 +1,17 @@
+/*Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+• Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+
+• Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)*/
+//===================================================================================================================================
+var alien_color1 = "green"; //'green', 'yellow', or 'red'
+if (alien_color1 === "green") {
+    console.log("The player just earned 5 points");
+}
+else
+    (null);
+var alien_color2 = "green"; //'green', 'yellow', or 'red'
+if (alien_color2 === "red") {
+    console.log("The player just earned 5 points");
+}
+else
+    (null);
