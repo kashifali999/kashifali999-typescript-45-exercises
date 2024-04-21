74eef4baef0713ea150261c8a5edd8cc8c0b7e7d
Kashif Ali <kashif_ali999@yahoo.com>
2024-04-21 14:22:32 +0500

    initial commit

---
+++ exe-23-conditional-tests/main23.js
@@ -0,0 +1,27 @@
+// Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+//===========================================================================================================================================
+var car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+console.log(car == 'toyota');
+var number = 10;
+console.log("Is number == 10 ? I predict True.");
+console.log(number === "10");
+console.log(number === 10);
+var bike = 'honda';
+console.log("Is bike == 'honda'? I predict True.");
+console.log(car == 'honda');
+console.log(car == 'super star');
+var city = 'Karachi';
+console.log("Is city == 'Karachi'? I predict True.");
+console.log(car == 'Lahore');
+console.log(car == 'Karachi');
+var fruit = 'Mango';
+console.log("Is fruit == 'Mango'? I predict True.");
+console.log(car == 'Mango');
+console.log(car == 'Apple');
