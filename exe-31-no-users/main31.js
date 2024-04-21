59e3bcd631c1562a57a01daee26bad01c537012c
Kashif Ali <kashif_ali999@yahoo.com>
2024-04-22 03:54:22 +0500

    initial commit

---
+++ exe-31-no-users/main31.js
@@ -0,0 +1,23 @@
+/*No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+• If the list is empty, print the message We need to find some users!
+
+• Remove all of the usernames from your array, and make sure the correct message is printed.*/
+//==========================================================================================================================================
+var users2 = [];
+if (users2.length === 0) {
+    console.log("We need to find some users");
+}
+else {
+    for (var i = 0; i < users2.length; i++) {
+        console.log(users2[i]);
+    }
+}
+var users1 = ["Shabbir", "Zaheer", "Ijaz", "Sarfraz"];
+if (users1.length === 0) {
+    console.log("We need to find some users");
+}
+else {
+    for (var i = 0; i < users1.length; i++) {
+        console.log(users1[i]);
+    }
+}
---
+++ exe-31-no-users/main31.ts
@@ -0,0 +1,16 @@
+/*No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+• If the list is empty, print the message We need to find some users!
+
+• Remove all of the usernames from your array, and make sure the correct message is printed.*/
+//==========================================================================================================================================
+let users2 : string [] = []
+if(users2.length === 0){console.log("We need to find some users")}
+else{
+    for(let i = 0 ; i < users2.length ; i++){console.log(users2[i])}
+}
+
+let users1 : string [] = ["Shabbir", "Zaheer", "Ijaz", "Sarfraz"]
+if(users1.length === 0){console.log("We need to find some users")}
+else{
+    for(let i = 0 ; i < users1.length ; i++){console.log(users1[i])}
+}
