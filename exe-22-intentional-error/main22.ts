dd4854e28707998e5ce2d9c8ca25b69ab1b036b9
Kashif Ali <kashif_ali999@yahoo.com>
2024-04-21 07:33:57 +0500

    initial commit

---
+++ exe-22-intentional-error/main22.ts
@@ -0,0 +1,7 @@
+// Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+//============================================================================================================================================
+let cities:string[] = ["Lahore", "Karachi", "Peshawer", "Quetta", "Islamabad"]
+console.log(cities[5]);
+console.log(cities[4]);
+
+
