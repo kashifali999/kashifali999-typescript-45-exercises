d0dd305aa4cd106f534f7fcefe3584d8c4a62755
Kashif Ali <kashif_ali999@yahoo.com>
2024-04-21 07:21:21 +0500

    initial commit

---
+++ exe-21-create-object/main21.js
@@ -0,0 +1,8 @@
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+//============================================================================================================================
+// Method # 01
+var myData1 = { firstName: "Kashif", lastName: "Ali", profession: "Teaching", age: "39" };
+console.log(myData1);
+// Method # 02
+var myData2 = { firstName: "Kashif", lastName: "Ali", profession: "Teaching", age: "39" };
+console.log(myData2);
---
+++ exe-21-create-object/main21.ts
@@ -0,0 +1,9 @@
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+//============================================================================================================================
+// Method # 01
+let myData1 = {firstName:"Kashif", lastName:"Ali", profession:"Teaching", age:"39"}
+console.log(myData1)
+
+// Method # 02
+let myData2:{firstName:string, lastName:string, profession:string, age:string} = {firstName:"Kashif", lastName:"Ali", profession:"Teaching", age:"39"}
+console.log(myData2)
