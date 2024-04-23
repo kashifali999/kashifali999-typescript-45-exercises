5c7c7bc581f5b22016bb599e782fc24597c1e16b
Kashif Ali <kashif_ali999@yahoo.com>
2024-04-24 04:17:12 +0500

    initial commit

--- exe-37-large-shirts/main37.js
+++ exe-37-large-shirts/main37.js
@@ -1,6 +1,8 @@
 /*Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.*/
 //==============================================================================================================================================
 function make_shirt(size, message) {
+    if (size === void 0) { size = "Large"; }
+    if (message === void 0) { message = "'I love TypeScript'"; }
     console.log("I am making T-shirts of ".concat(size, " size with a message ").concat(message, " printed on those"));
 }
-make_shirt("Large", "'I love TypeScript'");
+make_shirt("Small", "'I'm a coder'");
--- exe-37-large-shirts/main37.ts
+++ exe-37-large-shirts/main37.ts
@@ -1,6 +1,6 @@
 /*Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.*/
 //==============================================================================================================================================
-function make_shirt (size : string , message : string){
-    console.log(`I am making T-shirts of ${size} size with a message ${message} printed on those`)
+function make_shirt(size = "Large", message = "'I love TypeScript'"){
+    console.log("I am making T-shirts of ".concat(size, " size with a message ").concat(message, " printed on those"));
 }
-make_shirt("Large" , "'I love TypeScript'")
+make_shirt("Small", "'I'm a coder'");
