6f8b93281b114e380072862863e1f0f9fd1068ca
Kashif Ali <kashif_ali999@yahoo.com>
2024-04-26 06:25:42 +0500

    initial commit

--- exe-40-album/main40.ts
+++ exe-40-album/main40.ts
@@ -1,9 +1,29 @@
 /*Album: Write a function called make_album() that builds a Object describing a music album. The function should take in an artist name and an album title, and it should return a Object containing these two pieces of information. Use the function to make three dictionaries representing different albums. Print each return value to show that Objects are storing the album information correctly. Add an optional parameter to make_album() that allows you to store the number of tracks on an album. If the calling line includes a value for the number of tracks, add that value to the album’s Object. Make at least one new function call that includes the number of tracks on an album.*/
 //============================================================================================================================================
-function make_album(arist_name , album_title , ){}
-
-
-
+// function make_album(artist: string, title: string, tracks?: number): object {
+//     // Create an empty object to store album information
+//     const album: object = {};
+//     album["artist"] = artist;
+//     album["title"] = title;
+  
+//     // Add tracks property if provided
+//     if (tracks !== undefined) {
+//       album["tracks"] = tracks;
+//     }
+  
+//     return album;
+//   }
+  
+//   // Create three albums using the function
+//   const album1 = make_album("The Beatles", "Abbey Road");
+//   const album2 = make_album("Pink Floyd", "Dark Side of the Moon", 9);
+//   const album3 = make_album("Radiohead", "OK Computer");
+  
+//   // Print the album information
+//   console.log(album1); // { artist: "The Beatles", title: "Abbey Road" }
+//   console.log(album2); // { artist: "Pink Floyd", title: "Dark Side of the Moon", tracks: 9 }
+//   console.log(album3); // { artist: "Radiohead", title: "OK Computer" }
+  
 
 
 
